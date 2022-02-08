b26a9e3ff2507bd9671c8c2c302ab1c9544901d4
roberthughes <hughes.brewzz@gmail.com>
2022-02-08 11:02:06 -0700

    commit tree 3994d548bad2f8d96b77f57d652aa6d32ac4d105

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {
+ name: "Sam",
+ streetAddress: "8 lane",
+};
+employee;
+
+function updateEmployeeWithKeyAndValue(employee, name, Sam) {
+    console.log("these are my arguments", employee, name, Sam)
+    const updateEmployee = {...employee };
+
+    console.log("copy of employee", updateEmployee);
+
+    updateEmployee[employee] = 'Sam';
+
+    return updateEmployee;
+}
+// understand how many parameters
+// create function body and console log par..
+// what is expected to be returned...?
